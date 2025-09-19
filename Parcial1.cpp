d93b2675106c5ec33c74c7f13fa5f58dfadf37e6
Mate1592 <mate1502.me@gmail.com>
2025-09-19 00:58:43 +0000

    segundo commitazo

--- Parcial1.cpp
+++ Parcial1.cpp
@@ -1,1 +1,19 @@
+//Parcial 1 de ALSE Grupo 3
+//Integrantes:
+// David Mateo Espinel Ramos
+//Kaleth Stalin Sierra Aranguen
+
 #include<iostream>
+#include<cmath>
+#include<vector>
+
+
+// Definición de estructura para representar un punto en el plano
+struct Point
+{
+    int x;
+    int y;
+};
+
+//
+
