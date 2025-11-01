0d9447ba17e9f56d4f109d903d5b64928dc65fab
Mate1592 <mate1502.me@gmail.com>
2025-11-01 01:55:30 +0000

    makes

---
+++ shopping_cart/CMakeFiles/shopping_cart_exe.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shopping_cart_exe.dir/main.cpp.o"
+  "CMakeFiles/shopping_cart_exe.dir/main.cpp.o.d"
+  "CMakeFiles/shopping_cart_exe.dir/src/CarritoCompras.cpp.o"
+  "CMakeFiles/shopping_cart_exe.dir/src/CarritoCompras.cpp.o.d"
+  "CMakeFiles/shopping_cart_exe.dir/src/ItemCarrito.cpp.o"
+  "CMakeFiles/shopping_cart_exe.dir/src/ItemCarrito.cpp.o.d"
+  "CMakeFiles/shopping_cart_exe.dir/src/Producto.cpp.o"
+  "CMakeFiles/shopping_cart_exe.dir/src/Producto.cpp.o.d"
+  "CMakeFiles/shopping_cart_exe.dir/src/Usuario.cpp.o"
+  "CMakeFiles/shopping_cart_exe.dir/src/Usuario.cpp.o.d"
+  "shopping_cart_exe"
+  "shopping_cart_exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shopping_cart_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
